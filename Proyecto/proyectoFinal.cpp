fb2e4b0c7f34cef150a9d6f2a0fd4f130bf590c2
Eduardo Briceno <83840504+EduardoBriceno89@users.noreply.github.com>
2022-01-02 16:19:53 -0600

    Update proyectoFinal.cpp

--- Proyecto/proyectoFinal.cpp
+++ Proyecto/proyectoFinal.cpp
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "areas.h"
 
 int main(){
 	
 	//empezamos aqui//
+	int l, L;
 	
+	printf("ingrese lado uno\n");
+	scanf("%d",&l);
+	printf("ingrese lado dos\n");
+	scanf("%d",&L);
+	
+	printf("El area del cuadrado es: %d",areaCuadrado(l,L));
+	
+	return 0;
+	
+	system("pause");
 }

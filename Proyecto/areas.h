ec3b5331563387446da5fe0d1a62eb876d282381
Eduardo Briceno <83840504+EduardoBriceno89@users.noreply.github.com>
2022-01-03 15:07:20 -0600

    Update areas.h

--- Proyecto/areas.h
+++ Proyecto/areas.h
@@ -1,3 +1,13 @@
-int areaCuadrado(int l, int L){ //funcion principal
-	return(l * L); //retornamos la operacion
+float areaCuadrado(float l1){
+	return(l1 * 4);
+}
+float areaRectangulo(float b, float h){
+	return(b * h);
+}
+float areaTriangulo(float baseTriangulo, float alturaTringulo){
+	return((baseTriangulo * alturaTringulo)/2);
+}
+float areaCirculo(float pi, float radio){
+	pi = 3.1416;
+	return(pi * pow(radio,2));
 }

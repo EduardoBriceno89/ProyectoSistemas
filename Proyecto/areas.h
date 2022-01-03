43a41cc069259e2721e860216bf6de89e6bb75ef
Eduardo Briceno <83840504+EduardoBriceno89@users.noreply.github.com>
2022-01-03 14:16:31 -0600

    Update areas.h

--- Proyecto/areas.h
+++ Proyecto/areas.h
@@ -1,5 +1,3 @@
-int areaCuadrado(int l, int L); //declaramos la funcion con sus debidos parametros
-
 int areaCuadrado(int l, int L){ //funcion principal
 	return(l * L); //retornamos la operacion
 }

c385023c3b4104875a1d9aa6f7db2af23f165087
AdolfoL <50492594+AdolfoL@users.noreply.github.com>
2022-01-02 20:32:27 -0600

    Update proyectoFinal.cpp

--- Proyecto/proyectoFinal.cpp
+++ Proyecto/proyectoFinal.cpp
@@ -1,20 +1,70 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "areas.h" //libreria para areas
+int main()
+{
+    int opcion;
+    float area, l1, l2, radio, h, base;
 
-int main(){
-	
-	//empezamos aqui//
-	int l, L;
-	
-	printf("ingrese lado uno\n");
-	scanf("%d",&l);
-	printf("ingrese lado dos\n");
-	scanf("%d",&L);
-	
-	printf("El area del cuadrado es: %d",areaCuadrado(l,L));
-	
-	return 0;
-	
-	system("pause");
-}
+    printf("        MENU    \n");
+    printf(" 1- Cuadrado\n");
+    printf(" 2- Rectangulo\n");
+    printf(" 3- Triangulo\n");
+    printf(" 4- Ciruclo\n");
+    printf("Introduce una opcion: ");
+    scanf("%d", &opcion);
+
+       switch(opcion)
+       {
+           case 1:
+           {
+               printf("Introduce el lado: ");
+               scanf("%f", &l1);
+
+               area=l1*l1;
+               printf("El area es de: %f", area);
+
+               break;
+           }
+
+           case 2:
+           {
+               printf("Introduce el lado 1: ");
+               scanf("%f", &l1);
+
+               printf("Introduce el lado 2: ");
+               scanf("%f", &l2);
+
+               area=l1*l2;
+               printf("El area es de: %f", area);
+
+               break;
+           }
+
+           case 3:
+           {
+               printf("Introduce la base: ");
+               scanf("%f", &base);
+
+               printf("Introduce la altura: ");
+               scanf("%f", &h);
+
+               area=(base*h)/2;
+               printf("El area es de: %f", area);
+
+               break;
+           }
+
+           case 4:
+           {
+               printf("Introduce el radio: ");
+               scanf("%f", &radio);
+
+               area=(radio*radio)*3.14;
+               printf("El area es de: %f", area);
+
+               break;
+           }
+
+           default:
+               printf("La opcion no es correcta");
+       }
+   }

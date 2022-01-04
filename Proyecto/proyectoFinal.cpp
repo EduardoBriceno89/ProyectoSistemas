f47b1b3079ab2b32745573bf638610f9f21f2cbe
PapsChulo <83840504+EduardoBriceno89@users.noreply.github.com>
2022-01-04 13:12:53 -0600

    Update proyectoFinal.cpp

--- Proyecto/proyectoFinal.cpp
+++ Proyecto/proyectoFinal.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "areas.h"
+#include "figuras.h"
 
 int main()
 {
@@ -9,39 +9,37 @@ int main()
 	//empezamos aqui//
 
 	int opcion, opcionAreas;
-	float l1, b, h, volumen, pi, radio, baseTriangulo, alturaTriangulo;
+	float l, b, h, volumen, pi, radio, baseTriangulo, alturaTriangulo, ladoTriangulo, per;
 
 	do
 	{
 		printf("*********MENU**********\n");
 		printf("que calculo desea realizar?\n");
 		printf("1- Areas\n");
-		printf("2- Volumenes\n");
-		printf("3- Distancias\n");
-		printf("4- Salir\n");
+		printf("2- Salir\n");
 		printf("Introduce una opcion: \n");
 		scanf("%d", &opcion);
 
 		switch (opcion)
 		{
-		case 1: //case para areas//
-		{
-			system("CLS");
 
-			printf("******ESCOJA LA FIGURA*******\n");
-			printf("1-Cuadrado\n");
-			printf("2-Rectangulo\n");
-			printf("3-Triangulo\n");
-			printf("4-Circulo\n");
-			scanf("%d", &opcionAreas); //opcion para elegir la figura//
+		case 1:
+			system("cls");
+			printf("******SELECCIONE FIGURA*******\n");
+			printf("1- Cuadrado\n");
+			printf("2- Rectangulo\n");
+			printf("3- Triangulo\n");
+			printf("4- Circulo\n");
+			scanf("%d", &opcionAreas);
 
 			switch (opcionAreas)
-			{ //switch para hacer calculo de figura previamente seleccionada//
+			{
 			case 1:
-				printf("ingrese lado...\n");
-				scanf("%f", &l1);
+				printf("Ingrese lado...\n");
+				scanf("%f", &l);
 
-				printf("El area del cuadrado es: %f", areaCuadrado(l1));
+				printf("El area del Cuadrado es: %.2f cm^2\n", areaCuadrado(l)); //area
+				printf("El perimetro del Cuadrado es: %.2f cm\n", distanciaCuadrado(l));
 				break;
 			case 2:
 				printf("Ingrese base...\n");
@@ -49,48 +47,43 @@ int main()
 				printf("Ingrese altura...\n");
 				scanf("%f", &h);
 
-				printf("El area del rectangulo es: %f", areaRectangulo(b, h));
+				printf("El area del Rectangulo es: %.2f cm^2\n", areaRectangulo(b, h));
+				printf("El perimetro del Rectangulo es: %.2f cm\n", distanciaRectangulo(b, h));
 				break;
 			case 3:
 				printf("Ingrese base...\n");
 				scanf("%f", &baseTriangulo);
 				printf("Ingrese altura...\n");
 				scanf("%f", &alturaTriangulo);
+				printf("Ingrese lado de triangulo...\n");
+				scanf("%f", &ladoTriangulo);
 
-				printf("El area del circulo es: %f", areaTriangulo(baseTriangulo, alturaTriangulo));
-
+				printf("El area del Triangulo es: %.2f cm^2\n", areaTriangulo(baseTriangulo, alturaTriangulo));
+				printf("El perimetro del Triangulo es: %.2f cm\n", distanciaTriangulo(baseTriangulo, alturaTriangulo, ladoTriangulo));
 				break;
 			case 4:
-				printf("Ingrese valor de radio...\n");
+				printf("Ingrese radio...\n");
 				scanf("%f", &radio);
 
-				printf("El area del circulo es: %f", areaCirculo(pi, radio));
-
+				printf("El area del Circulo es: %2.f cm^2\n", areaCirculo(pi, radio));
+				printf("El perimetro del Circulo es: %.2f cm\n", distanciaCirculo(pi, radio));
 				break;
+
 			default:
-				printf("La opcion no es correcta");
+				printf("Ingrese una opcion correcta...");
+				break;
 			}
+
 			break;
-		}
 		case 2:
-		{ //volumenes
-
-			//pega tu codigo aqui Mario
+			printf("Saliste del programa...\n");
 			break;
-		}
-		case 3:
-		{ //distancias
-
-			break;
-		}
-		case 4:
-			printf("Saliste del programa...");
-			break;
-
 		default:
-			printf("La opcion no es correcta");
+			printf("ingrese una opcion correcta...\n");
 		}
-	} while (opcion != 4);
 
+	} while (opcion != 2);
+
+	system("pause");
 	return 0;
 }
